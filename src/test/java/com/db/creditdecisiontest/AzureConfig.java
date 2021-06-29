375b5e315569d4230926acf5b7a0f79152c0488f
rameshkumar2410 <44162583+rameshkumar2410@users.noreply.github.com>
2021-06-29 15:16:44 +0800

    Add files via upload
    Added Azure config details

---
+++ src/test/java/com/db/creditdecisiontest/AzureConfig.java
@@ -0,0 +1,25 @@
+package com.db.creditdecisiontest;
+
+public class AzureConfig {
+
+    public AzureConfig() {
+    }
+
+    public String sasUrl;
+    public String accountName;
+    public String accountKey;
+    public String containerName;
+
+    @Override
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        b.append(sasUrl)
+          .append("\n")
+          .append(accountName)
+          .append("\n")
+          .append(accountKey)
+          .append("\n")
+          .append(containerName);
+        return b.toString();
+    }
+}
